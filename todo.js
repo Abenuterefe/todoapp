705f4a057fe64161e7653ff53c4df7e5b20b2ea8
Abenezer Terefe <abenuterefe@gmail.com>
2025-03-13 12:07:16 +0300

    fixing css bugs

--- todo.js
+++ todo.js
@@ -2,14 +2,19 @@ const input=document.getElementById("taskInput")
 function addTask(){
     const task=document.createElement("li")
     const dlt=document.createElement("button")
+    const div = document.createElement("div")
+    div.className = "todo";
     dlt.className = "dlt";
     dlt.textContent = 'delete task';
     
     if (input.value === "") {
         alert("please insert a task");
       } else {
-    task.textContent=input.value
+    
+    div.textContent=input.value
+
     document.getElementById("taskList").appendChild(task)
+    task.appendChild(div)
     task.appendChild(dlt)
     input.value=""
     dlt.addEventListener("click", deleteTask);
